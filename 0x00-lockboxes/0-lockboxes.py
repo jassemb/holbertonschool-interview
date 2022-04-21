86d3e17fc6af4ce838bb42198c090dd86229025d
jassemb <benalijassem201@gmail.com>
2022-04-21 11:39:18 +0100

    interview

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,16 +9,17 @@ def canUnlockAll(boxes):
         Return True if ALL boxes can be opened, else return False.
     """
     unlocked_boxes_index = [0]
+
     for idx in range(len(boxes)):
         if idx in unlocked_boxes_index:
             for j in boxes[idx]:
                 unlocked_boxes_index.append(j)
                 if j < len(boxes):
-                    if boxes[j] != [] and type(boxes[j]) == list:
+                    if boxes[j] != [] and isinstance(boxes[j], list):
                         unlocked_boxes_index.extend(boxes[j])
                     if boxes[j] is None:
                         return False
-                    if type(boxes[j]) == int:
+                    if isinstance(boxes[j], int):
                         unlocked_boxes_index.append(boxes[j])
         else:
             return False
