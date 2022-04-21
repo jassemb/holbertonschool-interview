f6bab5d399c06d0df1e77a40c9a563c0eb2a58c8
jassemb <benalijassem201@gmail.com>
2022-04-21 14:27:03 +0100

    0-lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,23 +4,27 @@
 """
 
 
+def join(T, R):
+    res = []
+    for e in R:
+        res += T[e]
+    return res
+
+
 def canUnlockAll(boxes):
     """
         Return True if ALL boxes can be opened, else return False.
     """
     unlocked_boxes_index = [0]
+    index = 0
+    total = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total[index:]):
+            if j not in total:
+                total.append(j)
+                index += 1
+                added = True
 
-    for idx in range(len(boxes)):
-        if idx in unlocked_boxes_index:
-            for j in boxes[idx]:
-                unlocked_boxes_index.append(j)
-                if j < len(boxes):
-                    if boxes[j] != [] and isinstance(boxes[j], list):
-                        unlocked_boxes_index.extend(boxes[j])
-                    if boxes[j] is None:
-                        return False
-                    if isinstance(boxes[j], int):
-                        unlocked_boxes_index.append(boxes[j])
-        else:
-            return False
-    return True
+    return len(total) == len(boxes)
