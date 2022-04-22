ee665677da8219139c16c6b7aaa1e155b50d4811
jassemb <benalijassem201@gmail.com>
2022-04-22 10:09:52 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,28 +3,17 @@
     Method that determines if all the boxes can be opened.
 """
 
-
-def join(T, R):
-    res = []
-    for e in R:
-        res += T[e]
-    return res
-
-
 def canUnlockAll(boxes):
     """
         Return True if ALL boxes can be opened, else return False.
     """
-    unlocked_boxes_index = [0]
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
+    visted = set()
+    stack = [0]
+    while stack:
+        box = stack.pop()
+        visted.add(box)
+        for key in boxes[box]:
+            if key not in visted:
+                stack.append(key)
+    return len(visted)==len(boxes)
 
-    return len(total) == len(boxes)
