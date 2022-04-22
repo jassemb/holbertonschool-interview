e78d7664bf69175241d2d81d0205cfbae5b49fb7
jassemb <benalijassem201@gmail.com>
2022-04-22 10:31:17 +0100

    lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
 """
-    Method that determines if all the boxes can be opened.
+    Box opener method.
 """
 
 
 def canUnlockAll(boxes):
-    """
-        Return True if ALL boxes can be opened, else return False.
-    """
-    unlocked_boxes_index = [0]
+    key_list = [0]
 
-    for idx in range(len(boxes)):
-        if idx in unlocked_boxes_index:
-            for j in boxes[idx]:
-                unlocked_boxes_index.append(j)
-                if j < len(boxes):
-                    if boxes[j] != [] and isinstance(boxes[j], list):
-                        unlocked_boxes_index.extend(boxes[j])
-                    if boxes[j] is None:
-                        return False
-                    if isinstance(boxes[j], int):
-                        unlocked_boxes_index.append(boxes[j])
-        else:
+    for key in key_list:
+        for src in boxes[key]:
+            if src not in key_list and src < len(boxes):
+                key_list.append(src)
+    for index in range(len(boxes)):
+        if index not in key_list:
             return False
     return True
