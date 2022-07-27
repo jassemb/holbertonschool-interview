1142a635a05ce5d49bf10d1829b042163dd1bc3a
jassemb <benalijassem201@gmail.com>
2022-07-27 13:51:24 +0100

    N Queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -22,3 +22,25 @@ if not isinstance(int(sys.argv[1]), int):
     exit(1)
 
 n = int(sys.argv[1])
+
+def is_valid_state(state):
+    return True
+
+def get_candidates(state):
+    return []
+
+def search(state, solutions):
+    if is_valid_state(state):
+        solutions.append(state.copy())
+        # return
+
+    for candidate in get_candidates(state):
+        state.add(candidate)
+        search(state, solutions)
+        state.remove(candidate)
+
+def solve():
+    solutions = []
+    state = set()
+    search(state, solutions)
+    return solutions
