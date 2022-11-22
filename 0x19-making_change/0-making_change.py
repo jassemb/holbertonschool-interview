0a80f4769d3ce4028a961113a5eaa6d3e5a2cdfb
jassemb <benalijassem201@gmail.com>
2022-11-22 09:25:47 +0100

    making change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """
+    make change: bottom up method
+    """
+    if total <= 0:
+        return 0
+    if total in coins:
+        return 1
+    if len(coins) <= 0:
+        return -1
+    M = [0]*(total + 1)
+
+    for j in range(1, total + 1):
+        M[j]= total + 1
+
+    for i in range(1, total+1):
+        if(j >= M[i]):
+            minimum = min(minimum, 1+M[j-M[i]])
+        M[j] = minimum
+    return M[total] if M[total] != total + 1 else -1
+
+
