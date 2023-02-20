49bb83c6e4ca028237307b9f37a260d8266f1d21
jassem ben ali <85318125+jassemb@users.noreply.github.com>
2023-02-20 11:32:03 +0100

    fix code .

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -9,19 +9,17 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-    if (*str == '\0' && *pattern == '\0')
-        return (1);
-    if (*str == *pattern && *pattern != '*')
-        return (regex_match(str + 1, pattern + 1));
-    if (*pattern == '*')
-    {
-        if (*(pattern + 1) != '*' && *(pattern + 1) != '\0')
-        {
-            if (*str == '\0')
-                return (0);
-            return (regex_match(str + 1, pattern) || regex_match(str, pattern + 1));
-        }
-        return (regex_match(str, pattern + 1));
-    }
-    return (0);
+	int tmp = 0;
+
+	if (*str == '\0' && *pattern == '\0')
+		return (1);
+	if ((*str == *pattern || *pattern == '.') && *(pattern + 1) != '*')
+		return (regex_match(str + 1, pattern + 1));
+	if (*(pattern + 1) == '*')
+	{
+		if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+			tmp = regex_match(str + 1, pattern);
+		return (regex_match(str, pattern + 2) || tmp);
+	}
+	return (0);
 }
