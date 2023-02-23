babff0af521def74665af9f2076fde39fdee7d01
jassemb <benalijassem201@gmail.com>
2023-02-23 10:04:41 +0100

    lock boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""
-    Box opener method.
-"""
-
-
 def canUnlockAll(boxes):
-    key_list = [0]
-
-    for key in key_list:
-        for src in boxes[key]:
-            if src not in key_list and src < len(boxes):
-                key_list.append(src)
-    for index in range(len(boxes)):
-        if index not in key_list:
+    if type(boxes) is not list and len(boxes)<0:
+        return False
+    for box in boxes:
+        if type(box) is not list:
             return False
-    return True
+    
+    keys=[0]
+    for key in keys:
+            if type(key) is not int and key<0:
+                return False
+            else:              
+                #print(key)
+                for bk in boxes[key]:
+                    # print("===")                
+                     #print(boxes[key])
+
+                    if bk not in keys and bk < len(boxes):
+                        keys.append(bk)
+
+
+    if len(keys)== len(boxes):
+
+        return True
+    else :
+        return False
