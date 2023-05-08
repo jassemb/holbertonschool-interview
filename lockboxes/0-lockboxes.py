6c3bc430d970358539dcd2360bcc4cbdf67f5b79
jassemb <benalijassem201@gmail.com>
2023-05-08 12:05:52 +0100

    test

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Box opener method.
+"""
+
+def concat(A,B):
+    res=[]
+    for w in B:
+        res+=A[w]
+    return res
+
+def canUnlockAll(boxes):
+    index = 0
+    total = list(set(boxes[0])|{0})
+    sum = True
+    while sum:
+        sum=False
+        for j in concat(boxes,total[index:]):
+            if j not in total:
+                total.append(j)
+                index = index + 1
+                sum=True
+    print(total)
+    return len(total) == len(boxes)
+
