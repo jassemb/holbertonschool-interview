75dbf082c88d4863c7186d0d462585d4da85380c
jassemb <benalijassem201@gmail.com>
2023-06-27 12:49:34 +0100

    test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,10 +2,12 @@
 """
 	canUnlockAll : test if all boxes can be opened
 """
+
+
 def canUnlockAll(boxes):
     j = 0
-    while (j != len(boxes)-1):
-        for i in range(j+1, len(boxes)):
+    while (j != len(boxes) - 1):
+        for i in range(j + 1, len(boxes)):
             if (i in boxes[j]):
                 j += 1
             else:
