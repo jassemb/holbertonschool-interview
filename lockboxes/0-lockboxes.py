bdfa2d83893f699b93d5844e39614ed71dbc616b
jassem ben ali <85318125+jassemb@users.noreply.github.com>
2023-10-25 12:05:34 +0100

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,12 +5,15 @@ canUnlockAll : test if all boxes can be opened
 
 
 def canUnlockAll(boxes):
-    j = 0
-    while (j != len(boxes) - 1):
-        for i in range(j + 1, len(boxes)):
-            if (i in boxes[j]):
-                j += 1
-            else:
-                return (False)
-            break
-    return (True)
+	kList = [0]
+
+	for keys in kList:
+		for i in boxes[keys]:
+			if i not in kList and i < len(boxes):
+				kList.append(i)
+
+	for i in range(len(boxes)):
+		if i not in kList:
+			return False
+	return True
+
